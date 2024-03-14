6122051bd27c5d7b359e5cb6f739a459aad682c7
elkprostoelk <novikovmisha138138@gmail.com>
2024-03-15 01:00:28 +0200

    fixed expired token sending; added links to fanfic page

--- src/app/auth-interceptor/auth.interceptor.ts
+++ src/app/auth-interceptor/auth.interceptor.ts
@@ -7,15 +7,16 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tokenGetter } from "../app.module";
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private readonly jwtHelper: JwtHelperService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     let token = tokenGetter();
-    if (token) {
+    if (token && !this.jwtHelper.isTokenExpired(token)) {
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
